976d35262550a8ea06a51e752f6a16f8163c1449
tinytony0710 <145215344+tinytony0710@users.noreply.github.com>
2026-06-01 20:25:35 +0800

    .

--- src/data/servants.js
+++ src/data/servants.js
@@ -11,12 +11,12 @@ export const servants = [
         ],
         'skill': [
             {},//0->1 never used
-            {'gem_of_lancer': 5},//1->2
+            {'lancer': 5},//1->2
             {'gem_of_lancer': 12},
-            {'magic_lancer_monument': 5},
-            {'magic_lancer_monument': 12, 'dragon_fang': 12},
-            {'secret_lancer_monument': 5, 'dragon_fang': 24},
-            {'secret_lancer_monument': 12, 'great_knight_medal': 10},
+            {'magic_gem_of_lancer': 5},
+            {'magic_gem_of_lancer': 12, 'dragon_fang': 12},
+            {'secret_gem_of_lancer': 5, 'dragon_fang': 24},
+            {'secret_gem_of_lancer': 12, 'great_knight_medal': 10},
             {'great_knight_medal': 20, 'aurora_steel': 6},
             {'small_bell_of_absolution': 72, 'aurora_steel': 18},
             {},//9->10
@@ -25,10 +25,10 @@ export const servants = [
             {},//0->1 never used
             {'gem_of_lancer': 5},//1->2
             {'gem_of_lancer': 12},
-            {'magic_lancer_monument': 5},
-            {'magic_lancer_monument': 12, 'void_dust': 10},
-            {'secret_lancer_monument': 5, 'void_dust': 20},
-            {'secret_lancer_monument': 12, 'dragon_reverse_scale': 2},
+            {'magic_gem_of_lancer': 5},
+            {'magic_gem_of_lancer': 12, 'void_dust': 10},
+            {'secret_gem_of_lancer': 5, 'void_dust': 20},
+            {'secret_gem_of_lancer': 12, 'dragon_reverse_scale': 2},
             {'dragon_reverse_scale': 4, 'reactor_core_of_dawn': 4},
             {'scales_of_fantasy': 24, 'reactor_core_of_dawn': 11},
             {},//9->10
@@ -46,24 +46,24 @@ export const servants = [
         ],
         'skill': [
             {},//0->1 never used
-            {},//1->2
-            {},
-            {},
-            {},
-            {},
-            {},
+            {'gem_of_archer': 5, 'gem_of_lancer': 5, 'gem_of_rider': 5},//1->2
+            {'gem_of_saber': 5, 'gem_of_caster': 5, 'gem_of_assassin': 5, 'gem_of_berserker': 5},
+            {'magic_gem_of_archer': 5, 'magic_gem_of_lancer': 5, 'magic_gem_of_rider': 5},
+            {'magic_gem_of_saber': 5, 'magic_gem_of_caster': 5, 'magic_gem_of_assassin': 5, 'magic_gem_of_berserker': 5},
+            {'secret_gem_of_archer': 5, 'secret_gem_of_lancer': 5, 'secret_gem_of_rider': 5},
+            {'secret_gem_of_saber': 5, 'secret_gem_of_caster': 5, 'secret_gem_of_assassin': 5, 'secret_gem_of_berserker': 5},
             {'shell_of_reminiscence': 12},
             {'dragon_reverse_scale': 15},
             {},//9->10
         ],
         'extra_skill': [
             {},//0->1 never used
-            {},//1->2
-            {},
-            {},
-            {},
-            {},
-            {},
+            {'gem_of_archer': 5, 'gem_of_lancer': 5, 'gem_of_rider': 5},//1->2
+            {'gem_of_saber': 5, 'gem_of_caster': 5, 'gem_of_assassin': 5, 'gem_of_berserker': 5},
+            {'magic_gem_of_archer': 5, 'magic_gem_of_lancer': 5, 'magic_gem_of_rider': 5},
+            {'magic_gem_of_saber': 5, 'magic_gem_of_caster': 5, 'magic_gem_of_assassin': 5, 'magic_gem_of_berserker': 5},
+            {'secret_gem_of_archer': 5, 'secret_gem_of_lancer': 5, 'secret_gem_of_rider': 5},
+            {'secret_gem_of_saber': 5, 'secret_gem_of_caster': 5, 'secret_gem_of_assassin': 5, 'secret_gem_of_berserker': 5},
             {'great_knight_medal': 30},
             {'scales_of_fantasy': 30},
             {},//9->10
